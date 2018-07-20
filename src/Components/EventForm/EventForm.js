3b321267836eb56f3cd8e5af2e301cb97ec6a39a
Nathan Pabst <nathan.pabst@gmail.com>
2018-07-19 19:23:49 -0500

    added bootstrap to the form component. needs styling

--- src/Components/EventForm/EventForm.js
+++ src/Components/EventForm/EventForm.js
@@ -102,136 +102,109 @@ class EventForm extends React.Component {
     const {newEvent} = this.state;
     return (
       <div className="col-xs-8 col-xs-offset-2">
-        <h2 className="text-center">Add an event:</h2>
-        <form onSubmit={this.formSubmit}>
-          <div className="row">
-            <fieldset className="col-xs-3">
-              <label htmlFor="team1">Team One:</label>
-              <br />
-              <input
-                className="col-xs-12"
-                type="text"
-                id="team1"
-                placeholder="Chiefs"
-                value={newEvent.team1}
-                onChange={this.team1Change}
-              />
-            </fieldset>
-            <fieldset className="col-xs-3">
-              <label htmlFor="team1">Team Two:</label>
-              <br />
-              <input
-                className="col-xs-12"
-                type="text"
-                id="team2"
-                placeholder="Chargers"
-                value={newEvent.team2}
-                onChange={this.team2Change}
-              />
-            </fieldset>
-            <fieldset className="col-xs-3">
-              <label htmlFor="date">Date:</label>
-              <br />
-              <input
-                className="col-xs-12"
-                type="date"
-                id="date"
-                placeholder="9/9/18"
-                value={newEvent.date}
-                onChange={this.dateChange}
-              />
-            </fieldset>
-            <fieldset className="col-xs-3">
-              <label htmlFor="time">Time:</label>
-              <br />
-              <input
-                className="col-xs-12"
-                type="time"
-                id="time"
-                placeholder="3:05 PM"
-                value={newEvent.time}
-                onChange={this.timeChange}
-              />
-            </fieldset>
-            <fieldset className="col-xs-3">
-              <label htmlFor="location">Location:</label>
-              <br />
-              <input
-                className="col-xs-12"
-                type="text"
-                id="location"
-                placeholder="Double Dogs"
-                value={newEvent.location}
-                onChange={this.locationChange}
-              />
-            </fieldset>
-            <fieldset className="col-xs-3">
-              <label htmlFor="address">Address:</label>
-              <br />
-              <input
-                className="col-xs-12"
-                type="text"
-                id="address"
-                placeholder="12345 Main Street"
-                value={newEvent.address}
-                onChange={this.addressChange}
-              />
-            </fieldset>
-            <fieldset className="col-xs-3">
-              <label htmlFor="city">City:</label>
-              <br />
-              <input
-                className="col-xs-12"
-                type="text"
-                id="city"
-                placeholder="Nashville"
-                value={newEvent.city}
-                onChange={this.cityChange}
-              />
-            </fieldset>
-            <fieldset className="col-xs-3">
-              <label htmlFor="state">State:</label>
-              <br />
-              <input
-                className="col-xs-12"
-                type="text"
-                id="state"
-                placeholder="TN"
-                value={newEvent.state}
-                onChange={this.stateChange}
-              />
-            </fieldset>
-            <fieldset className="col-xs-3">
-              <label htmlFor="zip">Zip Code:</label>
-              <br />
-              <input
-                className="col-xs-12"
-                type="text"
-                pattern="[0-9]{5}"
-                id="zip"
-                placeholder="37209"
-                value={newEvent.zip}
-                onChange={this.zipChange}
-              />
-            </fieldset>
+        <h2 className="">Add an event:</h2>
+        <form className="form-horizontal" onSubmit={this.formSubmit}>
+          <div className="form-group">
+            <label htmlFor="team1">Team One:</label>
+            <input
+              type="text"
+              class="form-control"
+              id="team1"
+              placeholder="Chiefs"
+              value={newEvent.team1}
+              onChange={this.team1Change}
+            />
           </div>
-          <div className="row">
-            <fieldset className="col-xs-6">
-              <label htmlFor="description">Description:</label>
-              <br />
-              <input
-                className="col-xs-12"
-                type="text"
-                id="description"
-                placeholder="See you there!"
-                value={newEvent.description}
-                onChange={this.descriptionChange}
-              />
-            </fieldset>
+          <div className="form-group">
+            <label htmlFor="team2">Team Two:</label>
+            <input
+              type="text"
+              class="form-control"
+              id="team2"
+              placeholder="Chargers"
+              value={newEvent.team2}
+              onChange={this.team2Change}
+            />
           </div>
-          <button className="col-xs-6 btn btn-danger col-xs-offset-3">
-            Save event
-          </button>
+          <div className="form-group">
+            <label htmlFor="datetime-local">Date</label>
+            <input
+              type="datetime-local"
+              class="form-control"
+              id="date"
+              placeholder="MM/DD/YYYY"
+              value={newEvent.date}
+              onChange={this.dateChange}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="location">Location</label>
+            <input
+              type="text"
+              class="form-control"
+              id="location"
+              placeholder="Double Dogs"
+              value={newEvent.location}
+              onChange={this.locationChange}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="address">Address</label>
+            <input
+              type="text"
+              class="form-control"
+              id="address"
+              placeholder="1234 Main Street"
+              value={newEvent.address}
+              onChange={this.addressChange}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="city">City</label>
+            <input
+              type="text"
+              class="form-control"
+              id="city"
+              placeholder="Nashville"
+              value={newEvent.city}
+              onChange={this.cityChange}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="state">State</label>
+            <input
+              type="text"
+              class="form-control"
+              id="state"
+              placeholder="TN"
+              value={newEvent.state}
+              onChange={this.stateChange}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="zip">Zip Code</label>
+            <input
+              type="text"
+              pattern="[0-9]{5}"
+              class="form-control"
+              id="zip"
+              placeholder="37215"
+              value={newEvent.zip}
+              onChange={this.zipChange}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="description">Description</label>
+            <textarea
+              type="text"
+              class="form-control"
+              rows="3"
+              id="description"
+              placeholder="See you there!"
+              value={newEvent.description}
+              onChange={this.descriptionChange}
+            />
+          </div>
         </form>
       </div>
     );
