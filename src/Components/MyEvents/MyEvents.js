31234c93cca99d908e79a896bc0294bc57c47df8
Nathan Pabst <nathan.pabst@gmail.com>
2018-07-24 15:15:02 -0500

    thumbnails with buttons in place

--- src/Components/MyEvents/MyEvents.js
+++ src/Components/MyEvents/MyEvents.js
@@ -23,13 +23,20 @@ class MyEvents extends React.Component {
   render () {
     const {events} = this.state;
     const eventComponents = events.map((event) => (
-      <div key={event.id} className="card col-sm-4">
-        <div className="card-body">
-          <h3 className="card-title">{event.homeTeam} vs. {event.awayTeam}</h3>
-          <h4 className="card-text">{event.dateTime}</h4>
-          <p className="card-text">{event.location}</p>
-          <p className="card-text">{event.address}</p>
-          <p className="card-text">{event.city}, {event.state}</p>
+      <div key={event.id} className="row">
+        <div className="col-xs-6 col-md-3">
+          <div className="thumbnail eventThumbnail">
+            <img src="https://media.bizj.us/view/img/3507951/kansas-city-chiefs-mark*750xx681-384-192-110.jpg" alt="team-logo" />
+            <div className="caption">
+              <h3>{event.dateTime}</h3>
+              <h4>{event.homeTeam} vs. {event.awayTeam}</h4>
+              <p>{event.location}</p>
+              <p>{event.address}</p>
+              <p>{event.city}, {event.state}</p>
+              <p>{event.description}</p>
+              <p><button type="button" className="btn btn-primary">Edit</button> <button type="button" className="btn btn-default">Delete</button></p>
+            </div>
+          </div>
         </div>
       </div>
     ));
