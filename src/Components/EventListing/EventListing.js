ee371291fd3f63329139529e303fb1105d96d485
Nathan Pabst <nathan.pabst@gmail.com>
2018-07-30 19:04:28 -0500

    modified event listing to show team logos instead of team name. all events now showing

--- src/Components/EventListing/EventListing.js
+++ src/Components/EventListing/EventListing.js
@@ -3,7 +3,6 @@ import React from 'react';
 import './EventListing.css';
 
 import fbEvents from '../../firebaseRequests/events';
-import fbGames from '../../firebaseRequests/games';
 
 class EventListing extends React.Component {
   state = {
@@ -18,33 +17,22 @@ class EventListing extends React.Component {
       .catch((error) => {
         this.setState({events: []});
       });
-    fbGames.getAllGames()
-      .then((games) => {
-        this.setState({games});
-      })
-      .catch((error) => {
-        console.error('error retrieving games', error);
-      });
   }
   render () {
     const {events} = this.state;
     const eventComponents = events.map((event) => {
-      const game = this.state.games.find(x => x.id === event.gameId);
-      if (game) {
-        return (
-          <div key={event.id} className="card">
-            <div className="card-body">
-              <h3 className="card-title"><img className="team-logo-home" alt="teamLogo" src={game.aLogo}/> vs. <img className="team-logo-home" alt="teamLogo" src={game.hLogo}/></h3>
-              <h4 className="card-text">{event.dateTime}</h4>
-              <p className="card-text">{event.location}</p>
-              <p className="card-text">{event.address}</p>
-              <p className="card-text">{event.city}, {event.state}</p>
-              <p className="card-text">{event.details}</p>
-            </div>
+      return (
+        <div key={event.id} className="card">
+          <div className="card-body">
+            <h3 className="card-title"><img className="team-logo-home" alt="teamLogo" src={event.aLogo}/> vs. <img className="team-logo-home" alt="teamLogo" src={event.hLogo}/></h3>
+            <h4 className="card-text">{event.dateTime}</h4>
+            <p className="card-text">{event.location}</p>
+            <p className="card-text">{event.address}</p>
+            <p className="card-text">{event.city}, {event.state}</p>
+            <p className="card-text">{event.details}</p>
           </div>
-        );
-      }
-      return '';
+        </div>
+      );
     });
     return (
       <div className="Home">
