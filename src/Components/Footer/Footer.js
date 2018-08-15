b874b1f448b9b6fbd08117cfd29d6e68f86cc546
Nathan Pabst <nathan.pabst@gmail.com>
2018-08-14 19:59:10 -0500

    added new styling to the event form

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,9 +1,9 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
 
 import './Footer.css';
 
 class Footer extends React.Component {
+
   render () {
     return (
       <div className="footer">
